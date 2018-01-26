3bd817211bf754ef581945f3ab8529b15ae8a5ea
Adrian Garcia San Jose <adriangarciasanjose@gmail.com>
2018-01-26 09:18:13 +0100

    Creacion solo Game
    Crea la clase game desde el main

--- Lluvia_de_estrellas/src/lluvia_de_estrellas/Lluvia_de_estrellas.java
+++ Lluvia_de_estrellas/src/lluvia_de_estrellas/Lluvia_de_estrellas.java
@@ -15,7 +15,7 @@ public class Lluvia_de_estrellas {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Game juego=new Game();
     }
     
 }
