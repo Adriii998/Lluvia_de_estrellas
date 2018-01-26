9084beb6e2585817d92e06ec6502267bd8632377
Jorca82 <34540528+Jorca82@users.noreply.github.com>
2018-01-26 10:03:11 +0100

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,31 @@
+/*
+ * 
+ * By  Adrian Garcia San Jose.
+ * 
+ */
+package lluvia_de_estrellas;
+
+/**
+ *
+ * @author adri
+ */
+public class Game {
+    
+    /*
+      Jorge prueva pull
+    */
+    
+    int puntuacion;
+    int nivel;
+    
+	barramenu
+	
+    new Vista();
+    
+    new ControlMenu(vista);
+    new controlLetras(vista);
+    new Controlbarra(vista);
+    
+    
+    
+}
