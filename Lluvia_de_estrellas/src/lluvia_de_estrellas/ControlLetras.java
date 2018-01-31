6c141e1c52ad8738fe68a61b9e083db7f5d62b03
Adrian Garcia San Jose <adriangarciasanjose@gmail.com>
2018-01-31 09:23:35 +0100

    Estable elmina
    no tiene nada de colisiones, solo cae, no implementado la subida

--- Lluvia_de_estrellas/src/lluvia_de_estrellas/ControlLetras.java
+++ Lluvia_de_estrellas/src/lluvia_de_estrellas/ControlLetras.java
@@ -16,51 +16,52 @@ import javax.swing.Timer;
  * @author adri
  */
 public class ControlLetras implements KeyListener {
-    
+
     private NewLetras letras;
     private Game juego;
-    
+
     //timer que va a generar las letras cada cierto tiempo
     Timer timer;
-    
-    public ControlLetras(Game game){
-        timer=new Timer(1000,new ActionListener() {
+
+    public ControlLetras(Game game) {
+        timer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 nuevaLetra();
             }
         });
-        letras=new NewLetras(this);
-        this.juego=game;
+        letras = new NewLetras(this);
+
+        this.juego = game;
         game.vistaAddEscuchadorLetras(this);
         timer.start();
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-        String pulsada=(""+e.getKeyChar()).toUpperCase();
-        if(letras.teclaPulsada(pulsada)){
-            System.out.println("CORRECTA_________________________");
+        String pulsada = ("" + e.getKeyChar()).toUpperCase();
+        if (letras.teclaPulsada(pulsada)) {
+            System.out.println("CORRECTA");
             juego.eliminarLetra(pulsada);
-        }else{
+        } else {
             //juego.error();
         }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        
+
     }
-    
-    
-    public void nuevaLetra(){
-        System.out.println("nueva letra");
-        juego.mandarLetra(letras.getLetra());
+
+    public void nuevaLetra() {
+        if (letras.getTamañoUsadas() < letras.getTamañoPosibles()) {
+            juego.mandarLetra(letras.getLetra());
+        }
     }
-    
+
 }
--- Lluvia_de_estrellas/src/lluvia_de_estrellas/NewLetras.java
+++ Lluvia_de_estrellas/src/lluvia_de_estrellas/NewLetras.java
@@ -17,9 +17,11 @@ import javax.swing.Timer;
  * @author adri
  */
 public class NewLetras {
-    
+
+    private ControlLetras control;
+
     //private int heightVentana=800;
-    private int widthVentana=600;
+    private int widthVentana = 600;
 
     private static int HEIGHTPANEL = 50;
     private static int WIDTHPANEL = 50;
@@ -32,43 +34,39 @@ public class NewLetras {
     /*referencia al timer de cada cuanto se mueve una letra*/
     private Timer timerCaida;
 
-    private ControlLetras control;
-    
     private ArrayList<JButton> botones;
-    
+
     private JButton boton;
-    
 
     public NewLetras(ControlLetras ctrl) {
         this.control = ctrl;
+
         levels = new Niveles();
 
         posibles = levels.getLEVEL1();
         tiempoCaida = levels.getCAIDA1();
-        
-        timerCaida = new Timer(tiempoCaida,new ActionListener() {
+
+        timerCaida = new Timer(tiempoCaida, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 caer();
             }
         });
-        
-        botones=new ArrayList();
+
+        botones = new ArrayList();
         timerCaida.start();
     }
 
     public JButton getLetra() {
-
         boton = new JButton(generarLetra());
         //de momento es 50 por poner algo
         boton.setBounds(posXAleatoria(), 0, WIDTHPANEL, HEIGHTPANEL);
         botones.add(boton);
-        
         return boton;
     }
 
     /**
-     * 
+     *
      * @return retorna la letra que se ha generado de entre las posibles
      */
     private String generarLetra() {
@@ -78,12 +76,10 @@ public class NewLetras {
 
         //coge una letra
         //seleccionada = "" + posibles.charAt(getPosicion());
-
-        while (usadas.contains(seleccionada) && usadas.length()<posibles.length()) {
+        while (usadas.contains(seleccionada) && usadas.length() < posibles.length()) {
             //coge una letra
             seleccionada = "" + posibles.charAt(getPosicion());
-            System.out.println("entro en bucle");
-            
+
         }
         usadas += seleccionada;
         return seleccionada;
@@ -92,21 +88,22 @@ public class NewLetras {
     public int getPosicion() {
         int posicion; //posicion de la que se va a seleccionar la letra
         posicion = (int) (Math.random() * posibles.length());
+
         return posicion;
     }
-    
-    public int posXAleatoria(){
-        int x=(int) (Math.random()*widthVentana);
+
+    public int posXAleatoria() {
+        int x = (int) (Math.random() * widthVentana);
         return x;
     }
+
     /*
     public int posYAleatoria(){
         int y=(int) (Math.random()*heightVentana);
         return y;
     }*/
-    
     /**
-     * 
+     *
      * @param letra la letra pulsada en teclado
      * @return si es valida o no
      */
@@ -124,17 +121,25 @@ public class NewLetras {
 
         usadas = usadas.substring(0, indice) + usadas.substring(indice + 1);
     }
-    
+
+    //parte de movimiento de las letras
     /**
-     * @param 
+     * @param
      */
-    public void caer(){
+    public void caer() {
         for (JButton btn : botones) {
-            btn.setBounds(btn.getX(), btn.getY()+20, 50, 50);
+            btn.setBounds(btn.getX(), btn.getY() + 20, 50, 50);
         }
         //boton.setBounds(50, boton.getY()+10, 50, 50);
     }
-    
-    
+
+    //getter de tamaño usadas y posibles
+    public int getTamañoUsadas() {
+        return usadas.length();
+    }
+
+    public int getTamañoPosibles() {
+        return posibles.length();
+    }
 
 }
